81d998792a5c69477b1efe31c5a6fbd94065cb7c
buzuneh <buze.hj21@gamil.com>
2024-04-19 22:10:51 +0300

    make cart interactive

--- script/amazon.js
+++ script/amazon.js
@@ -76,7 +76,12 @@ if(matchingItem){
     quantity: 1
   });
 }  
-console.log(cart);
+let cartQuantity = 0;
+cart.forEach((item) => {
+  cartQuantity += item.quantity;
+});
+document.querySelector('.js-cart-quantity')
+.innerHTML=cartQuantity;
     });
 
   });
