94c50f987336567bfebdd076b304d0a78255fe6e
buzuneh <buze.hj21@gamil.com>
2024-04-19 21:44:38 +0300

    make cart uton interactive

--- script/amazon.js
+++ script/amazon.js
@@ -1,52 +1,11 @@
 
-let products=[{
-    image: '<img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg">',
-    name: 'Black and gray Atletics Cotton Sock -6 pairs',
-    rating:{
-        stars:4.5,
-        count:87 },
-         priceIncent:1090
-},
-    { image: '<img class="product-image" src="images/products/intermediate-composite-basketball.jpg">',
-    name: 'Intermediate Size Bascketball',
-    rating:{
-        stars:4.0,
-        count:127
-    },
-    priceIncent:2095 
-},
-    {  image:'<img class="product-image" src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">',
-        name: 'Plain Cotton T-Shirt - 2 Pack',
-        rating:{
-            stars:4.5,
-            count:56
-        }, 
-        priceIncent:799
-    },{
-      image:'<img class="product-image" src="images/products/acadamy book.jpg">',
-      name:'When Worlds Collide',
-rating:{
-  stars:4.5,
-  count:2000
-},
-priceIncent:851
-    },{
-      image:'<img class="product-image" src="images/products/closes-amazon.jpg">',
-      name:"'OYOANGLE Women's 3 Piece Pajama Set Sleepwear Loungewear Sets PJ Set Print Tee Top and Shorts with Pants'",
-rating:{
-  stars:4.5,
-  count:99
-},
-priceIncent:33.99
-    }
-  ];
-let productHTML = '';
+let productsHTML = '';
 products.forEach((product) => {
-    productHTML += `
+    productsHTML += `
     <div class="product-container">
     <div class="product-image-container">
     <img class="product-image"
-      ${product.image}
+    src="${product.image}"/>
     </div>
 
     <div class="product-name limit-text-to-2-lines">
@@ -63,7 +22,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-price">
-$${(product.priceIncent / 100).toFixed(2)}
+$${(product.priceCents / 100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
@@ -88,12 +47,36 @@ $${(product.priceIncent / 100).toFixed(2)}
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart" 
+    data-product-id ="${product.id}">
       Add to Cart
     </button>
   </div>
         `;
         });
 
-  console.log(productHTML);
-  document.querySelector('.js-product-grid').innerHTML = productHTML;
+  console.log(productsHTML);
+  document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+  document.querySelectorAll('.js-add-to-cart')
+  .forEach((button)=>{
+    button.addEventListener('click', ()=>{
+      const productId= button.dataset.productId;
+      let matchingItem;
+cart.forEach((item) => {
+  if(productId === item.productId){
+matchingItem = item;
+  }
+});
+if(matchingItem){
+  matchingItem.quantity += 1;
+}else{
+  cart.push({
+    productId:productId,
+    quantity: 1
+  });
+}  
+console.log(cart);
+    });
+
+  });
