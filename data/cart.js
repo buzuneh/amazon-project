29b1afd11b668a1968cfd9140c7c0d8cc8dcadad
buzuneh <buze.hj21@gamil.com>
2024-04-21 14:55:27 +0300

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,7 @@
-export let cart = [
-  {
+export let cart =JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=
+  [{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2
 },
@@ -8,6 +10,12 @@ export let cart = [
   quantity:1
 }
 ];
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function AddToCart(productId){
 
     let matchingItem;
@@ -24,6 +32,7 @@ export function AddToCart(productId){
         quantity: 1
       });
     }  
+    saveToStorage();
   }
   export function removeFromCart(productId){
     const newCart=[];
@@ -33,4 +42,5 @@ export function AddToCart(productId){
       }
     });
     cart = newCart;
+    saveToStorage();
   }
